d97bdd168e1f65421dbbdd9506a8f2386f2f199f
Luke MacLean <luke_maclean@yahoo.com>
2021-01-23 18:06:53 -0500

    adding atlas db back into the code.

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {
+mongoose.connect("mongodb+srv://capitallion:"+process.env.ATLASPW+"@cluster0.wstkh.mongodb.net/userDB", {
 
   //mongodb+srv://capitallion:"+process.env.ATLASPW+"@cluster0.wstkh.mongodb.net
 
